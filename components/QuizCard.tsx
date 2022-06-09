042d167bdf3eb00768738060723021eb3ced9265
Zach Neibaur <zneibaur@protonmail.com>
2022-06-09 11:39:32 -0600

    Giving styled-components another shot in Next.js

--- components/QuizCard.tsx
+++ components/QuizCard.tsx
@@ -1,6 +1,42 @@
 import { useState, useEffect, useContext } from 'react';
 import globalContext from '../context/globalContext';
-import styles from './QuizCard.module.css';
+import styled from 'styled-components';
+
+const CardWrapper = styled.div`
+  width: 90%;
+  padding: 15px 30px 30px;
+  background-color: #fff;
+  box-shadow: 0px 2px 4px 0px rgba(0,0,0,0.2);
+  border-radius: 5px;
+  margin: 50px auto;
+`
+
+const OptionsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px;
+`
+
+const OptionBtn = styled.button`
+  all: unset;
+  border: 2px solid #eee;
+  border-radius: 5px;
+  padding: 5px 10px;
+  font-size: 14px;
+  cursor: pointer;
+`
+
+const NextBtn = styled.button`
+  all: unset;
+  cursor: pointer;
+  border-radius: 5px;
+  border: 2px solid transparent;
+  color: #0A558C;
+  background-color: #DCEEFB;
+  padding: 5px 10px;
+  margin-top: 15px;
+  font-size: 14px;
+`
 
 interface CardProps {
   id: number;
@@ -48,16 +84,16 @@ export default function QuizCard({ id, text, answerOptions, answer, cardNumber, 
   }, [answer, selectedAnswer])
 
   return (
-    <div className={styles.cardWrapper}>
+    <CardWrapper>
       <h2>{id}</h2>
       <p>{text}</p>
-      <div className={styles.optionsWrapper}>
+      <OptionsWrapper>
         {answerOptions?.length > 0 && answerOptions.map((option, index) => (
-          <button className={styles.option} key={index} type="button" onClick={() => setSelectedAnswer(option.toString())}>{option.toString()}</button>
+          <OptionBtn key={index} type="button" onClick={() => setSelectedAnswer(option.toString())}>{option.toString()}</OptionBtn>
         ))}
-      </div>
-      {isAnswered && id !== numberOfQuestions && <button className={styles.nextBtn} type="button" onClick={handleNext}>NEXT</button>}
-      {selectedAnswer && id === numberOfQuestions &&<button className={styles.nextBtn} type="button" onClick={handleFinish}>FINISH</button>}
-    </div>
+      </OptionsWrapper>
+      {isAnswered && id !== numberOfQuestions && <NextBtn type="button" onClick={handleNext}>NEXT</NextBtn>}
+      {selectedAnswer && id === numberOfQuestions &&<NextBtn type="button" onClick={handleFinish}>FINISH</NextBtn>}
+    </CardWrapper>
   )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,24 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps (ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return initialProps;
-  }
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-  render () {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
+    try {
+      ctx.renderPage = () => 
+        originalRenderPage({
+          enhanceApp: (App) => props =>
+            sheet.collectStyles(<App {...props} />),
+        })
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 }
